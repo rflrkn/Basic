6ecf59219c23988086988ac89b59258c1b0ca5d8
Rafael Arkenau <Th3f@users.noreply.github.com>
2015-08-30 15:39:58 +0200

    Simplified and improved random.php
    Thanks to TheBlackPhantom, who wrote this little piece of code. He was too lazy to do a pull request, so I decided to simply add the code by myself. :D

--- random.php
+++ random.php
@@ -1,21 +1,4 @@
 <?php
-
-$random = array (
-    'img/header01.jpg',
-    'img/header02.jpg',
-    'img/header03.jpg',
-    'img/header04.jpg',
-    'img/header05.jpg',
-    'img/header06.jpg',
-    'img/header07.jpg',
-    'img/header08.jpg',
-    'img/header09.jpg',
-);
-
-$count = count($random) -1;
-
-$number = rand(0, $count);
-
-echo $random[$number]
-
+$random = array_diff(scandir(PATH.theme_url('img/')), ['.', '..']);
+echo 'img/'.$random[array_rand($random)];
 ?>
